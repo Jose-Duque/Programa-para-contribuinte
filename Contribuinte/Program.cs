25a0912f13f7ad6918503043e48ff19bb663dbe0
José Carlos Duque Filho <cj_duque@hotmail.com>
2020-12-04 12:48:08 -0300

    initil

---
+++ Contribuinte/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Contribuinte
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
